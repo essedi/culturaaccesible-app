b6f7f437350d961d28bc9ad0051674bcc97bc725
madeleine <madeleine@essedi.es>
2019-06-05 11:23:33 +0200

    added new download and enter icons

--- src/app/environment-variables/development.ts
+++ src/app/environment-variables/development.ts
@@ -1,5 +1,6 @@
 export const devVariables = {
     //baseUrl: 'http://192.168.0.102:4567',
-    baseUrl: "http://54.89.129.120:4567",
+    baseUrl: "http://54.198.129.130:4567",
+    
     environmentName: 'Development Environment'
 };
