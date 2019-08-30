f89d4eadfa022abfa4227d36a6e167f873dc39e5
Essedi Consulting <essediconsulting@iMac-de-Essedi.local>
2019-08-30 18:03:52 +0200

    ios improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -45,7 +45,10 @@ export class MyApp {
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
             this.statusBar.styleDefault();
-            
+		if(this.platform.is('ios'))
+{
+this.nav.setRoot('ExhibitionList');
+}            
         });
     }
     
