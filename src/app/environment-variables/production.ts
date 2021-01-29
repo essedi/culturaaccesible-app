051fda2940e4200ed54e31188c2eddd04435a1e1
Elisabet <josep@union.local>
2021-01-29 10:17:26 +0100

    new api

--- src/app/environment-variables/production.ts
+++ src/app/environment-variables/production.ts
@@ -1,6 +1,6 @@
 export const prodVariables = {
     //baseUrl: 'http://192.168.0.102:4567',
-    baseUrl: "http://54.198.129.130:4567",
+    baseUrl: "http://3.82.154.38:4567",
 
     environmentName: 'Production Environment'
 };
